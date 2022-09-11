bf27cc6b660366d2fdf8b6be056ec2a4a35f9383
Akshat Sinha <akshatsinha97@gmail.com>
2022-09-11 05:55:27 +0530

    fake view for guest user

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -34,6 +34,9 @@ function addCookieItem(productId, action){
             delete cart[productId];
         }
     }
+
+    document.cookie = "cart=" + JSON.stringify(cart) + ";domain=;path=/";
+    location.reload()
 }
 
 function updateUserOrder(productId, action){
